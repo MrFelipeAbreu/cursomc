fcd3c6e839800a08a6555162bb04e5e8ed1b8f23
Felipe Abreu <fgnabreu@gmail.com>
2018-07-30 17:16:39 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/felipeabreu/cursomc/CursomcApplication.java
+++ src/main/java/com/felipeabreu/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.felipeabreu.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.felipeabreu.cursomc.domain.Cidade;
 import com.felipeabreu.cursomc.domain.Cliente;
 import com.felipeabreu.cursomc.domain.Endereco;
 import com.felipeabreu.cursomc.domain.Estado;
+import com.felipeabreu.cursomc.domain.Pagamento;
+import com.felipeabreu.cursomc.domain.PagamentoComBoleto;
+import com.felipeabreu.cursomc.domain.PagamentoComCartao;
+import com.felipeabreu.cursomc.domain.Pedido;
 import com.felipeabreu.cursomc.domain.Produto;
+import com.felipeabreu.cursomc.domain.enums.EstadoPagamento;
 import com.felipeabreu.cursomc.domain.enums.TipoCliente;
 import com.felipeabreu.cursomc.repositories.CategoriaRepository;
 import com.felipeabreu.cursomc.repositories.CidadeRepository;
 import com.felipeabreu.cursomc.repositories.ClienteRepository;
 import com.felipeabreu.cursomc.repositories.EnderecoRepository;
 import com.felipeabreu.cursomc.repositories.EstadoRepository;
+import com.felipeabreu.cursomc.repositories.PagamentoRepository;
+import com.felipeabreu.cursomc.repositories.PedidoRepository;
 import com.felipeabreu.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,7 +44,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private EnderecoRepository enderecoRepository;
 	@Autowired
 	private ClienteRepository clienteRepository;
-
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -85,5 +96,21 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		clienteRepository.save(Arrays.asList(cli1));
 		enderecoRepository.save(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+				
+		pedidoRepository.save(Arrays.asList(ped1, ped2));
+		pagamentoRepository.save(Arrays.asList(pagto1, pagto2));
 	}
 }
