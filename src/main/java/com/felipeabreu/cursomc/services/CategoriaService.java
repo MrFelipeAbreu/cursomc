fb76c2a1ad87d850a0437bab1350ec4f782fafc1
Felipe Abreu <fgnabreu@gmail.com>
2018-07-31 18:34:19 -0300

    Listando apenas as categorias com o DTO

--- src/main/java/com/felipeabreu/cursomc/services/CategoriaService.java
+++ src/main/java/com/felipeabreu/cursomc/services/CategoriaService.java
@@ -1,5 +1,7 @@
 package com.felipeabreu.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -45,5 +47,9 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos relacionados!!!!");
 		}
 		
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
