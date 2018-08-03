37633d4d60de7fde5fd3eb0add69588a36d3faad
Felipe Abreu <fgnabreu@gmail.com>
2018-08-03 18:57:41 -0300

    Criando profile de teste

---
+++ src/main/java/com/felipeabreu/cursomc/config/TesteConfig.java
@@ -0,0 +1,27 @@
+package com.felipeabreu.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.felipeabreu.cursomc.repositories.CategoriaRepository;
+import com.felipeabreu.cursomc.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TesteConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+		@Bean
+		public boolean instantiateDataBase() throws ParseException {
+			
+			dbService.instantiateDataBase();
+			
+			return true;
+		}
+}
